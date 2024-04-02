1721a15c9b20f23561653f4ff2b5080d2561101e
mlkctn <melik_cetin@hotmail.com>
2024-04-02 15:11:35 +0200

    "when you lose show the words"

--- Hangman-python/main.py
+++ Hangman-python/main.py
@@ -1,53 +1,42 @@
 import random
-
 from hangman_words import word_list
+from hangman_art import logo, stages
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
-
 end_of_game = False
 lives = 6
+guessed_letters = []
 
-
-from hangman_art import logo
 print(logo)
 
-
-display = []
-for _ in range(word_length):
-    display += "_"
+display = ["_" for _ in range(word_length)]
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-
-    if guess in display:
+    if guess in guessed_letters:
         print(f"You've already guessed {guess}")
+        continue
 
+    guessed_letters.append(guess)
 
-    for position in range(word_length):
-        letter = chosen_word[position]
-
-        if letter == guess:
-            display[position] = letter
-
-
-    if guess not in chosen_word:
-
+    if guess in chosen_word:
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+    else:
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-        
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print("You lose. The word was:", chosen_word)
 
- 
-    print(f"{' '.join(display)}")
+    print(' '.join(display))
 
- 
     if "_" not in display:
         end_of_game = True
         print("You win.")
 
-    from hangman_art import stages
     print(stages[lives])
